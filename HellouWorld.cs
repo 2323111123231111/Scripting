375160f302d1e413142c2bf95eb6bbf58e6773fa
2323111123231111 <120502038+2323111123231111@users.noreply.github.com>
2022-12-13 20:58:42 +0300

    Add files via upload

---
+++ HellouWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellouWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Вывод на консоль привет мир!
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
